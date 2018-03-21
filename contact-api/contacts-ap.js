d3162ab8e097103bd6a66d7b7f84981fa1749fef
James McDowell <jamesnmcdowell@gmail.com>
2018-03-21 10:58:18 -0400

    convert to es6

--- contact-api/contacts-ap.js
+++ contact-api/contacts-ap.js
@@ -1,60 +1,44 @@
 let http = require('http');
 
 let contacts = [
-    {id: 0, first: 'James', last: 'Dougal', email: 'james@gmail.com'},
+    { id: 0, first: 'James', last: 'Dougal', email: 'james@gmail.com' },
     { id: 1, first: 'Rebecca', last: 'Martin', email: 'rebecca@gmail.com' },
     { id: 2, first: 'Laura', last: 'Love', email: 'laura@gmail.com' },
 ];
 
 let lastId = 0;
 
-let findContact = function(id) {
+let findContact = id => {
     id = parseInt(id, 10);
-    return contacts.find(function(contact) {
-        return contact.id === id;
-    });
+    return contacts.find(contact => contact.id === id);
 };
 
-let deleteContact = function(contactToDelete) {
-    contacts = contacts.filter(function(contact) {
-        return contact !== contactToDelete;
-    });
+let deleteContact = contactToDelete => {
+    contacts = contacts.filter(contact => contact !== contactToDelete);
 };
 
-let readBody = function(request, callback) {
+let readBody = (request, callback) => {
     let body = '';
-    request.on('data', function(chunk) {
-        body += chunk.toString();
-    });
-    request.on('end', function() {
-        callback(body);
-    });
+    request.on('data', chunk => { body += chunk.toString() });
+    request.on('end', () => { callback(body) });
 };
 
-let matches = function(request, method, path) {
-    return request.method === method &&
-           request.url.startsWith(path);
-};
+let matches = (request, method, path) => request.method === method && request.url.startsWith(path);
 
-let getSuffix = function(fullUrl, prefix) {
-    return fullUrl.slice(prefix.length);
-};
+let getSuffix = (fullUrl, prefix) => fullUrl.slice(prefix.length);
 
-let getContactsRoute = function (request, response) {
-    response.end(JSON.stringify(contacts));
-};
+let getContactsRoute = (request, response) => { response.end(JSON.stringify(contacts)) };
 
-let postContactsRoute = function (request, response) {
-    readBody(request, function (body) {
+let postContactsRoute = (request, response) => {
+    readBody(request, (body) => {
         let contact = JSON.parse(body);
         contact.id = ++lastId;
-        console.log(contact);
         contacts.push(contact);
         response.end('Created contact!');
     });
 };
 
-let deleteContactRoute = function (request, response) {
+let deleteContactRoute = (request, response) => {
     let id = getSuffix(request.url, '/contacts/');
     let contact = findContact(id);
     deleteContact(contact);
@@ -62,16 +46,16 @@ let deleteContactRoute = function (request, response) {
     response.end('Deleted contact!');
 };
 
-let getContactRoute = function (request, response) {
+let getContactRoute = (request, response) => {
     let id = getSuffix(request.url, '/contacts/');
     let contact = findContact(id);
     response.end(JSON.stringify(contact));
 };
 
-let putContactRoute = function (request, response) {
+let putContactRoute = (request, response) => {
     let id = getSuffix(request.url, '/contacts/');
     let contact = findContact(id);
-    readBody(request, function (body) {
+    readBody(request, (body) => {
         let newParams = JSON.parse(body);
         console.log(newParams);
         Object.assign(contact, newParams);
@@ -79,7 +63,7 @@ let putContactRoute = function (request, response) {
     });
 };
 
-let notFound = function (request, response) {
+let notFound = (request, response) => {
     response.statusCode = 404;
     response.end('404, nothing here!');
 };
@@ -92,15 +76,9 @@ let routes = [
     { method: 'POST', path: '/contacts', handler: postContactsRoute },
 ];
 
-let server = http.createServer(function (request, response) {
+let server = http.createServer((request, response) => {
     console.log(request.method, request.url);
-
-    let route = routes.find(function (route) {
-        return matches(request, route.method, route.path);
-    });
-
+    let route = routes.find(route => matches(request, route.method, route.path));
     (route ? route.handler : notFound)(request, response);
-
 });
-
 server.listen(3000);
